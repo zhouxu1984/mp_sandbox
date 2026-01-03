aa2d63025097d8eee0968eaf013295e75ba3217a
xuzhou <xuzhou1865@163.com>
2026-01-03 11:06:06 +0800

    xuzhou logo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,15 @@
 // index.js
 Page({
+  data:{
+    wording: "xxx"
+  },
+
+  onclick: function(){
+    this.setData({
+      wording: "xuzhou"
+    })
+  },
+
   onLoad(options){
     console.log("小程序加载完成！");
   }
